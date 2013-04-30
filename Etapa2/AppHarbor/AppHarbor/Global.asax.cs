ce9281b398542135930c1e5de5864750ae4ddb68
isel-31612 <31612@alunos.isel.ipl.pt>
2013-04-30 19:27:30 +0100

    Removido EF para tentar isolar problema

--- Etapa2/AppHarbor/AppHarbor/Global.asax.cs
+++ Etapa2/AppHarbor/AppHarbor/Global.asax.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -36,7 +34,7 @@ namespace AppHarbor
             AreaRegistration.RegisterAllAreas();
 
             // Use LocalDB for Entity Framework by default
-            Database.DefaultConnectionFactory = new SqlConnectionFactory(@"Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
+            //Database.DefaultConnectionFactory = new SqlConnectionFactory(@"Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);

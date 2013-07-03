44c8cdd19861194b99b2a45326f4e8a4947041a2
isel-31612 <31612@alunos.isel.ipl.pt>
2013-07-03 19:59:05 +0100

    removido error handling

--- Etapa2/AppHarbor/AppHarbor/Global.asax.cs
+++ Etapa2/AppHarbor/AppHarbor/Global.asax.cs
@@ -18,7 +18,7 @@ namespace AppHarbor
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+           // filters.Add(new HandleErrorAttribute());
         }
 
         public static void RegisterRoutes(RouteCollection routes)
